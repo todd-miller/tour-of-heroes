73c620d0e95271b693b1037c0f82682e3b9c92fa
Todd Miller <todd_miller@skiplist.com>
2023-01-03 16:35:06 -0500

    removed unused lines from test file

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,15 +1,9 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { HeroService } from './hero.service';
 import { MessagesService } from './messages.service';
-import { Hero } from './hero';
 
-const MOCK_DATA = [
-  { id: 1, name: 'Hulk'},
-  { id: 2, name: 'Thor'},
-  { id: 3, name: 'Iron Man'},
-] as Hero[];
 
 describe('HeroService', () => {
   let heroService: HeroService;
