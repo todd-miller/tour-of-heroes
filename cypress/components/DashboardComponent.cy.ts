742ec4d5f18bb986ffde42ba0d87cd0162772b56
Todd Miller <todd_miller@skiplist.com>
2023-01-04 16:04:40 -0500

    mounting 2x different components

--- cypress/components/DashboardComponent.cy.ts
+++ cypress/components/DashboardComponent.cy.ts
@@ -4,27 +4,37 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { TestBed } from '@angular/core/testing';
 import { of, Observable } from 'rxjs';
 
-import { HeroDetailComponent } from '../../src/app/hero-detail/hero-detail.component';
+import { DashboardComponent } from '../../src/app/dashboard/dashboard.component';
+import { HeroSearchComponent } from '../../src/app/hero-search/hero-search.component';
 import { Hero } from '../../src/app/hero';
 import { HeroService } from '../../src/app/hero.service';
 
-describe('HeroDetailComponent.cy.ts', () => {
+const HEROES: Hero[] = [
+  { id: 2, name: 'Peter' },
+  { id: 10, name: 'Foo' },
+  { id: 3, name: 'Tiga' },
+  { id: 4, name: 'Bar' },
+]
+
+describe('DashboardComponent.cy.ts', () => {
   let heroService;
 
   it('mount', () => {
     cy.log('Test Start', cy.url());
-    
+
+    // Need to stub the service this is calling on init!
     TestBed.overrideProvider(HeroService, {
       useValue: {
-        getHero(id: number): Observable<Hero> {
-          const hero: Hero = { name: "Capt. Obvious", id: 10 };
-          return of(hero);
+        getHeroes(): Observable<Hero[]> {
+          return of(HEROES);
         } 
       }
-    }); 
-    cy.mount(HeroDetailComponent, {
+    });
+
+    cy.mount(DashboardComponent, {
       declarations: [ 
-        HeroDetailComponent,
+        DashboardComponent,
+        HeroSearchComponent
       ],
       imports: [
         RouterTestingModule.withRoutes([]),
