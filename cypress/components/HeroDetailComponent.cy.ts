9b8fcafbfb1202f4826dc92b3c4e772e655d88b8
Todd Miller <foxsan@protonmail.com>
2023-06-14 14:53:15 -0400

    updated cypress test file

--- cypress/components/HeroDetailComponent.cy.ts
+++ cypress/components/HeroDetailComponent.cy.ts
@@ -1,5 +1,4 @@
-import { FormsModule } from '@angular/forms';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms'; import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TestBed } from '@angular/core/testing';
 import { of, Observable } from 'rxjs';
